f99c9c04f9743d0f1b8220c7d2d493a206b2dabd
arnold626 <31412937+arnold626@users.noreply.github.com>
2017-09-05 14:29:20 +0800

    1.0.5

---
+++ app/src/main/java/com/saccl/test1/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.saccl.test1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+// http://www.coderefer.com/android-splash-screen-example-tutorial/
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
